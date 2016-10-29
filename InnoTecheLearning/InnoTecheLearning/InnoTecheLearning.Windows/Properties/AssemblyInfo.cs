a64594f9c330047f35e2cb6cb819bcc989064655
Happypig375 <hadrianwttang@outlook.com>
2016-10-29 23:11:14 +0800

    InnoTecheLearning.UWP

--- InnoTecheLearning/InnoTecheLearning/InnoTecheLearning.Windows/Properties/AssemblyInfo.cs
+++ InnoTecheLearning/InnoTecheLearning/InnoTecheLearning.Windows/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Innovative Technology Society of CSWCSS")]
 [assembly: AssemblyProduct("InnoTecheLearning")]
-[assembly: AssemblyCopyright("Copyright © Innovative Technology Society of CSWCSS 2015")]
+[assembly: AssemblyCopyright("Copyright © Innovative Technology Society of CSWCSS 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
