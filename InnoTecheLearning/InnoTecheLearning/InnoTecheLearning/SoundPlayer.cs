8ed03b40e8b385ce8d455a2d65c08e30301daea0
Happypig375 <hadrianwttang@outlook.com>
2016-11-02 22:33:35 +0800

    SoundPlayer

---
+++ InnoTecheLearning/InnoTecheLearning/InnoTecheLearning/SoundPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace InnoTecheLearning
+{
+    interface ISoundPlayer
+    { }
+    class SoundPlayer : ISoundPlayer
+    {
+#if __IOS__
+#elif __ANDROID__
+#elif NETFX_CORE
+        MediaElement _mediaElement;
+        public async Task Play(string FileName)
+        {
+            Windows.Storage.StorageFolder folder =
+            await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(folderName);
+            Windows.Storage.StorageFile file = await folder.GetFileAsync(fileName);
+            Windows.Storage.Streams.IRandomAccessStream stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
+            _mediaElement = new MediaElement
+            {
+                IsMuted = false,
+                Position = new TimeSpan(0, 0, 0),
+                Volume = 1
+            };
+            _mediaElement.SetSource(stream, file.ContentType);
+
+            await new Task(() =>
+            {
+                _mediaElement.Play();
+            });
+        }
+#endif
+    }
+}
