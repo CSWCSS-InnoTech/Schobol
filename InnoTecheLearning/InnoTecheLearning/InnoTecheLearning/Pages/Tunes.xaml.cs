10004caffb99d01a4da63a12ad2074578b226c78
Happypig375 <hadrianwttang@outlook.com>
2017-08-01 23:11:44 +0800

    Tunes Page rewrited in XAML

--- InnoTecheLearning/InnoTecheLearning/InnoTecheLearning/Pages/Tunes.xaml.cs
+++ InnoTecheLearning/InnoTecheLearning/InnoTecheLearning/Pages/Tunes.xaml.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using static InnoTecheLearning.Utils.Create;
+using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions.Implementations;
+using Plugin.MediaManager.Abstractions;
+using Plugin.MediaManager.Abstractions.Enums;
+
 namespace InnoTecheLearning.Pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +21,40 @@ namespace InnoTecheLearning.Pages
 		public Tunes ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            Violin.BackgroundColor = Color.Transparent;
+            Violin.Source = ImageSource(ImageFile.Violin);
+            Violin.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(_ => DisplayAlert("Violin", "🎻♫♬♩♪♬♩♪♬", "Beautiful"))
+            });
+            Cello.BackgroundColor = Color.Transparent;
+            Cello.Source = ImageSource(ImageFile.Cello);
+            Cello.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(_ => DisplayAlert("Cello", "🎻♫♬♩♪♬♩♪♬", "Beautiful"))
+            });
+
+            var OriginalColor = ViolinG.BackgroundColor;
+            var AllButtons = new[] { ViolinG, ViolinD, ViolinA, ViolinE, CelloC, CelloG, CelloD, CelloA };
+            foreach(var b in AllButtons)
+            {
+                b.Clicked += (sender, e) =>
+                {
+                    for (int j = 0; j < AllButtons.Length; j++) AllButtons[j].BackgroundColor = OriginalColor;
+                    CrossMediaManager.Current.Play(new MediaFile());
+                };
+            }
+        }
+        class A : IMediaFile
+        {
+            public MediaFileType Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public ResourceAvailability Availability { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public IMediaFileMetadata Metadata { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Url { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public bool MetadataExtracted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public event MetadataUpdatedEventHandler MetadataUpdated;
+        }
+    }
 }
